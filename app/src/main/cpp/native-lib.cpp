474b9567186d3d0180d99d9768db27be9b30dc69
jasonxiao <274692646@qq.com>
2016-10-21 21:49:03 +0800

    add ffmpeg

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,16 @@
 #include <jni.h>
 #include <string>
 
+extern "C"{
+    #include "libavcodec/avcodec.h"
+};
+
 extern "C"
 jstring
 Java_cn_jx_easyplayer_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    char info[10000] = { 0 };
+    sprintf(info, "%s\n", avcodec_configuration());
+    return env->NewStringUTF(info);
 }
