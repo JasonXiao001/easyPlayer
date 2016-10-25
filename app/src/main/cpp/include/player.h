e8f78fe13545a734d9467706f82bd6ebc84b152c
jasonxiao <274692646@qq.com>
2016-10-25 20:52:31 +0800

    add play video

---
+++ app/src/main/cpp/include/player.h
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2016/10/24.
+//
+
+#ifndef EASYPLAYER_PLAYER_H
+#define EASYPLAYER_PLAYER_H
+
+
+class player {
+
+};
+
+
+#endif //EASYPLAYER_PLAYER_H
---
+++ app/src/main/cpp/player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Administrator on 2016/10/24.
+//
+
+#include "player.h"
